f6c96024587c3068442afa42794bd1e514bb274f
SimonSaysGiveMeSmile <tianjiahe11@gmail.com>
2025-07-22 06:01:42 -0700

    Remove all files from repository

--- src/pages/JobListPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ShowJobList from '../features/ShowJob/components/ShowJobList';
-
-const JobsPage: React.FC = () => {
-  return <ShowJobList />;
-};
-
-export default JobsPage;
